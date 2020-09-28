cdc5b8fc9b0bd7e8bd7382cab95b342c2d70d28f
xiaoting.wang <xiaoting.wang@b2bpas.com>
2020-09-28 13:46:38 +0800

    提交

---
+++ src/components/HelloWorld1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{state.count}}*2={{double}}</h1>
+        <button @click="add">累加</button>
+    </div>
+</template>
+<script>
+import { reactive, computed } from 'vue';
+export default {
+    setup() {
+        const state = reactive({
+            count: 1
+        })
+        function add () {
+            state.count++
+        }
+        const double = computed(() => state.count*2)
+        return { state, add, double }
+    }
+}
+</script>
